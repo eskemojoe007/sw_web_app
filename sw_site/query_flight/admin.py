9fc33dab1a9cbcee5e63ec6dd9eaf779b616780d
Folkner, David Erik <david.folkner@ge.com>
2018-04-10 18:45:34 -0400

    Kinda working airports

--- sw_site/query_flight/admin.py
+++ sw_site/query_flight/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Airport
 
 # Register your models here.
+class AirportAdmin(admin.ModelAdmin):
+    # fields = ['pub_date','question_text']
+    fieldsets = [
+    ('Title', {'fields':['title','abrev','sw_airport']}),
+    ('Location Information', {'fields':['timezone','lattitude','longitude']})
+    ]
+    list_display = ('__str__', 'sw_airport','get_state')
+    list_filter = ['sw_airport']
+    search_fields = ['title']
+admin.site.register(Airport,AirportAdmin)
--- sw_site/query_flight/models.py
+++ sw_site/query_flight/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from timezone_field import TimeZoneField
+import reverse_geocoder as rg
 
-# Create your models here.
+
+class Airport(models.Model):
+    title = models.CharField(verbose_name='Long Name of Airport',max_length=50)
+    timezone = TimeZoneField(default='US/Eastern')
+    abrev = models.CharField(verbose_name='Airport Abreviation Code',max_length=4,primary_key=True)
+    lattitude = models.FloatField()
+    longitude = models.FloatField()
+    sw_airport = models.BooleanField(verbose_name='Southwest Airport')
+
+    sw_airport.admin_order_field = 'title'
+
+    def __str__(self):
+        return self.title + " - " + self.abrev
+
+    def get_state(self):
+        return rg.search((self.lattitude,self.longitude))[0]['admin1']
+
+    def get_country(self):
+        return rg.search((self.lattitude,self.longitude))[0]['cc']
+
+    get_state.short_description = 'State'
