2050924c5def427c457366cc8be61d5183c2e6da
Folkner, David Erik <david.folkner@ge.com>
2018-04-25 08:37:37 -0400

    Not working airport view

--- sw_site/query_flight/views.py
+++ sw_site/query_flight/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Airport, Flight
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Query Flight index.")
+
+
+def airport(request,airport_id):
+    airport = get_object_or_404(Airport,pk=airport_id)
+    return render(request,'query_flight/airport.html')
