1270abaac3fbac0aa8e3871e2a4bf729d3648fa2
eskemojoe007 <David.Folkner@gmail.com>
2018-05-16 21:06:58 -0400

    added quick get test

--- sw_site/query_flight/tests/query_flight/test_api.py
+++ sw_site/query_flight/tests/query_flight/test_api.py
@@ -5,11 +5,7 @@ from rest_framework.test import APITestCase, APIClient
 from query_flight.models import Airport, Flight, Layover
 import pytz
 
-class TestAirport(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.url = reverse('query_flight:airports-list')
-
+class check_status(APITestCase):
     def check_get(self,response):
         self.assertEqual(response.status_code, status.HTTP_200_OK,
             'Expected Response Code 200, received {} instead.'
@@ -18,6 +14,11 @@ class TestAirport(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED,
             'Expected Response Code 201, received {} instead.'
             .format(response.status_code))
+
+class TestAirport(check_status):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('query_flight:airports-list')
 
     def test_airport_list(self):
         response = self.client.get(self.url)
@@ -39,22 +40,9 @@ class TestAirport(APITestCase):
         self.assertEqual(Airport.objects.get().timezone.zone,'US/Eastern')
         self.assertEqual(Airport.objects.get().timezone,pytz.timezone('US/Eastern'))
 
-        # response = self.client.get(self.url)
-    # def test_get_single_airport(self):
-    #     response = self.client.get(reverse('query_flight:airports-detail',args=['ATL']))
-    #     self.check_get(response)
-    #     self.assertEqual(response.data.get('abrev'),'ATL')
-    #     self.assertEqual(response.data.get('title'),'Atlanta')
-    # def test_update_airport(self):
-    #     pass
-    # def test_partial_update_airport(self):
-    #     pass
-    # def test_destroy_airport(self):
-    #     pass
-
-    # @pytest.mark.django_db
-    # def test_airport_list_real():
-    #     client = APIClient()
-    #     response = client.get(reverse('query_flight:airports-list'))
-    #     assert response.status_code == 200
-    #     assert len(response.json()) > 0
+        # Can't be its own test for some unkown reason
+        # def test_get_airport(self):
+        response = self.client.get(reverse('query_flight:airports-detail',args=['ATL']))
+        self.check_get(response)
+        self.assertEqual(response.data.get('title'),'Atlanta')
+        self.assertEqual(response.data.get('timezone'),'US/Eastern')
